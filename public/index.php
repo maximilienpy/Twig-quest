b9e28e2839d70f2972ac2497e2a5d1fe5ae8a5b6
maximilienpy <max314116@gmail.com>
2020-11-14 22:18:01 +0100

    twig quest

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+require '../vendor/autoload.php';
+
+$loader = new Twig\Loader\FilesystemLoader('../src/View');
+$twig = new Twig\Environment($loader);
+
+
+$products = ['product1', 'product2', 'product3', 'product4', 'product5'];
+
+echo $twig->render('index.html.twig', array('products' =>$products));
+
+
+
+
+
